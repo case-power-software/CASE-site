b5099b3d43227961e849f2c98882c6c0aee9fe07
case-power-software <92675119+case-power-software@users.noreply.github.com>
2025-11-17 11:39:17 +0100

    Create contact.h

---
+++ contact.h
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <link rel="icon" href="case.ico" type="image/x-icon">
+  <title>Contact</title>
+</head>
+  
+<body>
+  <h2>Contact</h2>
+  <a href="mailto:case.power.software@gmail.com">Email Support</a>
+</body>
+
+</html>
